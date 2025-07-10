372a58277f1b3a528f09b6232faa9dcacb431504
r-parkhi <radhika.pparkhi@gmail.com>
2025-07-10 14:21:58 -0700

    feat: update grocery list

--- GroceryList.py
+++ GroceryList.py
@@ -4,37 +4,60 @@ class GroceryList:
   def __init__(self):
     self.items = {}
 
-  def addItem(self, name, quantity, category):
+
+  # Add item to list
+  def addItem(self, name, quantity, category=None):
+    # add quantities
     if name in self.items:
-      self.items[name]["quantity"] += (quantity) # BROKEN, DOES NOT ADD INTEGERS
-    else :
+      (self.items[name]["quantity"]) = int(self.items[name]["quantity"]) + int(quantity)
+
+    # create new key value pairs & print item name
+    else:
       self.items[name] = {
+        # "quantity": int(quantity),
         "quantity": quantity,
         "category": category,
       }
-    print(name + " added")
+    print(f"\n'{name}' added!")
 
+
+  # Remove item from list
   def removeItem(self, name):
+    # delete item & print "removed"
     if name in self.items:
       del self.items[name]
-      print(name + " removed")
+      print(f"\n'{name}' removed!")
 
-  def displayList(self):
-    if not self.items:
-      print("Your grocery list is empty!")
+    # print "not in list"
     else:
-      print("\nYour grocery list:")
+      print(f"\n'{name}' is not in your list!")
+
+
+  # Display list
+  def displayList(self):
+    # print list
+    if self.items:
+      print("\n\tYour grocery list:")
       for name, details in self.items.items():
         category = details.get("category")
         quantity = details.get("quantity")
-        print(name + "(" + category + ")" + ": " + str(quantity))
+        print("\t - " + name + " (" + category + ")" + ": " + str(quantity))
 
+    # print "empty"
+    else:
+      print("\nYour list is empty!")
+
+
+  # Find item in list
   def findItem(self, name):
+    # print details
     if name in self.items:
-      print("Found!")
+      print("\nFound:")
       details = self.items[name]
       category = details.get("category")
       quantity = details.get("quantity")
-      print(name + "(" + category + ")" + ": " + str(quantity))
+      print("- " + name + " (" + category + ")" + ": " + str(quantity))
+      
+    # print "not found"
     else:
-      return name + "not found"
+      print(f"\n'{name}' was not found in your list!")
